cfabf033344d2e0224b917d1e1d06cb166d92a72
Sárándi <sarandi.balint@gmail.com>
2022-10-11 11:19:14 +0200

    masodik feladat

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-let lista = [];
-let otteloszthato = [];
+let t = [];
+let t5 = [];
 
 document.addEventListener('DOMContentLoaded', () => {
     for (let i = 0; i < 20; i++) {
-        lista.push(Math.floor(Math.random() * 30) + 1)
+        t.push(Math.floor(Math.random() * 30) + 1)
     }
-    for (let i = 0; i < lista.length; i++) {
-        if (lista[i] % 5 == 0) {
-            otteloszthato.push(lista[i])
-            console.log(lista[i])
-        }
-    }
+    t5 = t.filter(e => e%5==0);
+    console.log(t5);
+    document.getElementById("masodik").addEventListener('click', () => {
+        t5 = t.filter(e => e%5==0);
+        console.log(t5);
+    });
 });
